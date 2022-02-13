3b78be9f5525abfb140521ee4b1b43e0175864b1
Anthony Immenschuh <60157977+anthonyimmen@users.noreply.github.com>
2022-02-13 11:24:32 -0600

    Add files via upload

---
+++ userInput.js
@@ -0,0 +1,43 @@
+/*
+Darren Fisher
+user data gathering and sending to Hedera
+
+*/
+
+let score;
+
+// get value from userInput
+document.getElementById("myButton").onclick = function () {
+  let score = document.getElementById("score").value;
+  let email = document.getElementById("email").value;
+  let wallet = document.getElementById("wallet").value;
+  window.open("mailto:test@example.com?subject=Please change this text to: NFT potential winner &body=Wallet ID: xxxxxxxx \n High Score: xxx \n then hit send!");
+};
+
+console.log("hello", score);
+
+let encrptyLUL;
+
+for (let x = 0; score.length; x++) {
+  if ((score[x] = 1)) {
+    encrptyLUL = encrptyLUL + a;
+  } else if ((score[x] = 2)) {
+    encrptyLUL = encrptyLUL + b;
+  } else if ((score[x] = 3)) {
+    encrptyLUL = encrptyLUL + c;
+  } else if ((score[x] = 4)) {
+    encrptyLUL = encrptyLUL + d;
+  } else if ((score[x] = 5)) {
+    encrptyLUL = encrptyLUL + e;
+  } else if ((score[x] = 6)) {
+    encrptyLUL = encrptyLUL + f;
+  } else if ((score[x] = 7)) {
+    encrptyLUL = encrptyLUL + g;
+  } else if ((score[x] = 8)) {
+    encrptyLUL = encrptyLUL + h;
+  } else if ((score[x] = 9)) {
+    encrptyLUL = encrptyLUL + i;
+  } else {
+    encrptyLUL = encrptyLUL + j;
+  }
+}
